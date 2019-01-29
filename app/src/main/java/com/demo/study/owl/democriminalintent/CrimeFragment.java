337fc67328afe40e57eac80760dcf99ca8526755
Owl <13170019@qq.com>
2019-01-29 19:18:46 +0800

    FragmentManager（未完成）

--- app/src/main/java/com/demo/study/owl/democriminalintent/CrimeFragment.java
+++ app/src/main/java/com/demo/study/owl/democriminalintent/CrimeFragment.java
@@ -9,11 +9,16 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 public class CrimeFragment extends Fragment {
     private Crime crime;
     private EditText titleEditText;
+    private Button dateButton;
+    private CheckBox solvedCheckBox;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -29,6 +34,8 @@ public class CrimeFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_crime, container, false);
 
         titleEditText = v.findViewById(R.id.crime_title);
+        dateButton = v.findViewById(R.id.crime_date);
+        solvedCheckBox = v.findViewById(R.id.crime_solved);
 
         titleEditText.addTextChangedListener(new TextWatcher() {
             @Override
@@ -47,7 +54,15 @@ public class CrimeFragment extends Fragment {
             }
         });
 
+        dateButton.setText(crime.getDate().toString());
+        dateButton.setEnabled(false);
 
+        solvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                crime.setSolved(isChecked);
+            }
+        });
         return v;
     }
 }
